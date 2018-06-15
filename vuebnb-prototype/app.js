ffbad40766aec6d99c5551fed4674442a2b798c7
Abbah Anoh <anohabbah@gmail.com>
2018-06-15 11:02:16 +0200

    vuebnb prototype complete

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        },
+        amenities: sample.amenities,
+        prices: sample.prices,
+        contracted: true,
+        modalOpen: false
+    },
+    watch: {
+        modalOpen: function (newValue) {
+            const className = 'modal-open';
+            newValue ? document.body.classList.add(className) : document.body.classList.remove(className);
+        }
+    },
+    methods: {
+        escapeKeyListener: function (evt) {
+            if (evt.keyCode === 27 && this.modalOpen) {
+                this.modalOpen = false;
+            }
+        }
+    },
+    created: function () {
+        document.addEventListener('keyup', this.escapeKeyListener);
+    },
+    destroy: function () {
+        document.removeEventListener('keyup', this.escapeKeyListener);
+    }
+});
